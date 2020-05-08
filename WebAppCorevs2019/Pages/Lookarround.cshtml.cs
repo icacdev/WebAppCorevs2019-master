4de4d35bd06964391063ac460aa39f51ce57946b
Icav Dev <icacdev@gmail.com>
2020-05-08 03:34:29 +0100

    updating nav links, lookarround added

---
+++ WebAppCorevs2019/Pages/Lookarround.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppCorevs2019.Pages
+{
+    public class LookarroundModel : PageModel
+    {
+        private readonly ILogger<LookarroundModel> _logger;
+
+        public LookarroundModel(ILogger<LookarroundModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
